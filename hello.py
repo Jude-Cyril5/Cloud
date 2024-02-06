1cdbc106511cb05f4ca731ace71828a3ab1b8969
Jude_Cyril5 <judejude0082@gmail.com>
2024-02-06 06:14:36 +0530

    added successfully

---
+++ hello.py
@@ -0,0 +1,23 @@
+a=int(input())
+res = [int(x) for x in str(a)]
+
+b=[]
+c=[]
+for i in res:
+    if (i%2)==0:
+        b.append(i)
+        
+    else:
+        c.append(i)
+print(b)
+print(c)
+sum =0
+rum =0
+sum =[]
+rum =[]
+for j in range (b):
+    sum =
+    
+
+
+    
